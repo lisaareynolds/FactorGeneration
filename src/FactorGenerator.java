f6c643bedeeab88bc828b2c03728505de094d20e
bobefett-qwert12 <32529872+bobefett-qwert12@users.noreply.github.com>
2017-12-13 18:56:10 -0700

    Update FactorGenerator.java

--- src/FactorGenerator.java
+++ src/FactorGenerator.java
@@ -1,10 +1,18 @@
 public class FactorGenerator {
     private int numberToFactor;
 
+    /**
+     * Creates a new FactorGenerator object
+     * @param numberToFactor the number to be factored
+     */
     public FactorGenerator(int numberToFactor) {
         this.numberToFactor = numberToFactor;
     }
 
+    /**
+     * checks whether there exist any factors of the number
+     * @return returns true or false
+     */
     public boolean hasMoreFactors(){
         boolean hasFactors = false;
         for(int i = 2; i <= this.numberToFactor; i++){
@@ -14,7 +22,11 @@ public class FactorGenerator {
         }
         return hasFactors;
     }
-
+    
+    /**
+     * Finds the value of the smallest factor, then divides the original number by that factor
+     * @return the smallest factor
+     */
     public int nextFactor(){
         int factor = 0;
         for(int i = 2; i <= this.numberToFactor; i++){
